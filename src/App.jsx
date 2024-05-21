a603fc1ad9ac8093bd3fc767e6001d3f6c639b15
huitopia <huitopia27@gmail.com>
2024-05-21 11:17:36 +0900

    🚚 : /, /write 경로와 Home, BoardWrite 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./page/member/Home.jsx";
+import { BoardWrite } from "./page/board/BoardWrite.jsx";
 
-function App(props) {
-  return <div></div>;
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+      children: [
+        {
+          path: "write",
+          element: <BoardWrite />,
+        },
+      ],
+    },
+  ]);
+  return (
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  );
 }
 
 export default App;
---
+++ src/page/board/BoardWrite.jsx
@@ -0,0 +1,5 @@
+import { Box } from '@chakra-ui/react';
+
+export function BoardWrite() {
+  return <Box>글 작성</Box>;
+}
