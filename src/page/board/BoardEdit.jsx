7cfa6283c29ead55b224dc464aae2e5ca33dbf05
huitopia <huitopia27@gmail.com>
2024-05-21 16:51:03 +0900

    ✨ : update button > modal > toast 출력 및 이동

--- src/page/board/BoardEdit.jsx
+++ src/page/board/BoardEdit.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import {
@@ -7,19 +7,51 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Spinner,
   Textarea,
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
 
 export function BoardEdit() {
   const { id } = useParams();
   const [board, setBoard] = useState(null);
+  const toast = useToast();
+  const navigate = useNavigate();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   useEffect(() => {
     axios.get(`/api/board/${id}`).then((res) => setBoard(res.data));
   }, []);
 
   function handleClickUpdate() {
-    axios.put(`/api/board/edit`, board);
+    axios
+      .put("/api/board/edit", board)
+      .then(() => {
+        toast({
+          status: "success",
+          description: "Update Success",
+          position: "top",
+        });
+        navigate(`/board/${board.id}`);
+      })
+      .catch((err) => {
+        if (err.response.status === 400) {
+          toast({
+            status: "error",
+            description: "Error!",
+            position: "top",
+          });
+        }
+      })
+      .finally(() => {
+        onClose();
+      });
   }
 
   if (board == null) {
@@ -57,8 +89,20 @@ export function BoardEdit() {
         </FormControl>
       </Box>
       <Box>
-        <Button onClick={handleClickUpdate}>Update</Button>
+        <Button onClick={onOpen}>Update</Button>
       </Box>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay>
+          <ModalContent>
+            <ModalHeader>{board.id}번 게시물</ModalHeader>
+            <ModalBody>Update?</ModalBody>
+            <ModalFooter>
+              <Button onClick={onClose}>Cancel</Button>
+              <Button onClick={handleClickUpdate}>Update</Button>
+            </ModalFooter>
+          </ModalContent>
+        </ModalOverlay>
+      </Modal>
     </Box>
   );
 }
