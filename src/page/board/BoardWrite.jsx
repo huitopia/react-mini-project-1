810908f25651cf10dc9369348e95ea12f259446a
huitopia <huitopia27@gmail.com>
2024-05-21 11:36:34 +0900

    ✨ : 글작성 화면 작업

--- src/page/board/BoardWrite.jsx
+++ src/page/board/BoardWrite.jsx
@@ -1,5 +1,55 @@
-import { Box } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import axios from "axios";
 
 export function BoardWrite() {
-  return <Box>글 작성</Box>;
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [writer, setWriter] = useState("");
+
+  function handleSaveClick() {
+    axios.post("/api/board/add", {
+      title,
+      content,
+      writer,
+    });
+  }
+
+  return (
+    <Box>
+      <Box>글 작성 화면</Box>
+      <Box>
+        <Box>
+          <FormControl>
+            <FormLabel>title</FormLabel>
+            <Input onChange={(e) => setTitle(e.target.value)} />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>contents</FormLabel>
+            <Textarea onChange={(e) => setContent(e.target.value)} />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>writer</FormLabel>
+            <Input onChange={(e) => setWriter(e.target.value)} />
+          </FormControl>
+        </Box>
+        <Box>
+          <Button colorScheme={"blue"} onClick={handleSaveClick}>
+            save
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
