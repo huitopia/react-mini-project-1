a31e05eaeb503d6997432c2726d01db9ebb283ff
huitopia <huitopia27@gmail.com>
2024-05-23 11:45:34 +0900

    ✨ : 별명이 없거나 기존 별명과 같으면 별명 중복 확인 버튼 비활성화

--- src/page/member/MemberEdit.jsx
+++ src/page/member/MemberEdit.jsx
@@ -5,6 +5,8 @@ import {
   FormHelperText,
   FormLabel,
   Input,
+  InputGroup,
+  InputRightElement,
   Modal,
   ModalBody,
   ModalContent,
@@ -23,6 +25,8 @@ export function MemberEdit() {
   const [member, setMember] = useState(null);
   const [oldPassword, setOldPassword] = useState("");
   const [passwordCheck, setPasswordCheck] = useState();
+  const [isCheckedNickName, setIsCheckedNickName] = useState(true);
+  const [oldNickName, setOldNickName] = useState("");
   const { id } = useParams();
   const toast = useToast();
   const navigate = useNavigate();
@@ -34,6 +38,7 @@ export function MemberEdit() {
       .then((res) => {
         const member1 = res.data;
         setMember({ ...member1, password: "" });
+        setOldNickName(member1.nickName);
       })
       .catch(() => {
         toast({
@@ -55,14 +60,41 @@ export function MemberEdit() {
   if (member === null) {
     return <Spinner />;
   }
+  let isDisableNickNameCheckButton = false;
+  if (member.nickName === oldNickName || member.nickName.length == 0) {
+    isDisableNickNameCheckButton = true;
+  }
 
   let isDisableSaveButton = false;
   if (member.password !== passwordCheck) {
     isDisableSaveButton = true;
   }
 
-  if (member.nickName.trin().length === 0) {
+  if (member.nickName.trim().length === 0) {
     isDisableSaveButton = true;
+  }
+
+  function handleCheckNickName() {
+    axios
+      .get(`/api/member/check?nickName=${member.nickName}`)
+      .then((res) => {
+        toast({
+          status: "warning",
+          description: "사용할 수 없는 별명입니다.",
+          position: "top",
+        });
+      })
+      .catch((err) => {
+        if (err.response.status === 404) {
+          toast({
+            status: "info",
+            description: "사용할 수 있는 별명입니다.",
+            position: "top",
+          });
+          setIsCheckedNickName(true);
+        }
+      })
+      .finally();
   }
 
   return (
@@ -100,12 +132,23 @@ export function MemberEdit() {
         </Box>
         <Box>
           <FormControl>별명</FormControl>
-          <Input
-            onChange={(e) =>
-              setMember({ ...member, nickName: e.target.value.trim() })
-            }
-            value={member.nickName}
-          />
+          <InputGroup>
+            <Input
+              onChange={(e) =>
+                setMember({ ...member, nickName: e.target.value.trim() })
+              }
+              value={member.nickName}
+            />
+            <InputRightElement w={"75px"} mr={1}>
+              <Button
+                isDisabled={isDisableNickNameCheckButton}
+                size={"sm"}
+                onClick={handleCheckNickName}
+              >
+                중복확인
+              </Button>
+            </InputRightElement>
+          </InputGroup>
         </Box>
         <Box>
           <Button
