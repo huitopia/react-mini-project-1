f5a74573ed7b9c7ffbb8f21bdb4d6289a5d22b96
huitopia <huitopia27@gmail.com>
2024-05-31 14:07:06 +0900

    ✨ : 본인 댓글만 수정 가능

--- src/component/comment/CommentItem.jsx
+++ src/component/comment/CommentItem.jsx
@@ -19,9 +19,12 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
+import { useContext } from "react";
+import { LoginContext } from "../LoginProvider.jsx";
 
 export function CommentItem({ comment, isProcessing, setIsProcessing }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const account = useContext(LoginContext);
   const toast = useToast();
   function handleRemoveClick() {
     setIsProcessing(true);
@@ -61,35 +64,40 @@ export function CommentItem({ comment, isProcessing, setIsProcessing }) {
           <Flex ml={3}>
             <Box>{comment.comment}</Box>
             <Spacer />
-            <Box>
-              <Button
-                size={"xs"}
-                variant="ghost"
-                colorScheme={"red"}
-                onClick={onOpen}
-                isLoading={isProcessing}
-              >
-                <FontAwesomeIcon icon={faXmark} />
-              </Button>
-            </Box>
+            {account.hasAccess(comment.memberId) && (
+              <Box>
+                <Button
+                  size={"xs"}
+                  variant="ghost"
+                  colorScheme={"red"}
+                  onClick={onOpen}
+                  isLoading={isProcessing}
+                >
+                  <FontAwesomeIcon icon={faXmark} />
+                </Button>
+              </Box>
+            )}
           </Flex>
         </Box>
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay>
-            <ModalContent>
-              <ModalHeader>삭제 확인</ModalHeader>
-              <ModalBody>댓글 삭제?</ModalBody>
-              <ModalFooter>
-                <ButtonGroup size={"xs"} variant="outline">
-                  <Button onClick={onClose}>cancel</Button>
-                  <Button colorScheme={"red"} onClick={handleRemoveClick}>
-                    delete
-                  </Button>
-                </ButtonGroup>
-              </ModalFooter>
-            </ModalContent>
-          </ModalOverlay>
-        </Modal>
+        {/* comment delete modal */}
+        {account.hasAccess(comment.memberId) && (
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay>
+              <ModalContent>
+                <ModalHeader>삭제 확인</ModalHeader>
+                <ModalBody>댓글 삭제?</ModalBody>
+                <ModalFooter>
+                  <ButtonGroup size={"xs"} variant="outline">
+                    <Button onClick={onClose}>cancel</Button>
+                    <Button colorScheme={"red"} onClick={handleRemoveClick}>
+                      delete
+                    </Button>
+                  </ButtonGroup>
+                </ModalFooter>
+              </ModalContent>
+            </ModalOverlay>
+          </Modal>
+        )}
       </Box>
     </Center>
   );
